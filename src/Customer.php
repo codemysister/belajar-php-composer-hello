7faf6dbac171c5413809e2d1f98275a8a72de08c
codemysister <theprojessa@gmail.com>
2021-08-30 17:11:50 +0700

    Add default value

--- src/Customer.php
+++ src/Customer.php
@@ -1,15 +1,16 @@
-<?php 
+<?php
 
 namespace Theprojessa\Belajar;
 
-class Customer {
+class Customer
+{
 
     public function __construct(private string $name)
     {
-        
     }
 
-    public function sayHello(string $name){
+    public function sayHello(string $name = "Guest")
+    {
         return "Hello $name, My name is $this->name";
     }
 }
